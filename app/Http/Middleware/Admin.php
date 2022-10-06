933c44e4cf111fe9f6c9f36dbe8621d68e00788c
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-10-05 19:25:19 -0500

    a

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        switch(auth::user()->rol){
+            case ('user'):
+                return redirect('user');
+                break;
+            case ('admin'):
+                return $next($request);
+                break;
+        }
+    }
+}
---
+++ app/Http/Middleware/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class User
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        switch(auth::user()->rol){
+            case ('admin'):
+                return redirect('admin');
+                break;
+            case ('user'):
+                return $next($request);
+                break;
+        }
+    }
+}
